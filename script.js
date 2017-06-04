1563339fd2f55e546c9c603bf0ddc3d3a297f102
JamesKim <james.kim.nz92@gmail.com>
2017-06-05 01:17:35 +1200

    changed location api to https

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var weatherData;
 var tempKelvin;
 var tempCelcius;
 var tempFarenheit;
+var isCelcius = true;
 
 var APIKey = "d93ea9433d5445c3dcae1dc6351df11a"; // I have to expose the key on Client for this FreeCodeCamp Project.
 
@@ -15,14 +16,14 @@ $(document).ready(function () {
     });
 });
 
-$('').click(function () {
-
+$('.button').click(function () {
+    toggleUnits();
 });
 
 function getLocation() {
     return new Promise((resolve, reject) => {
         var request = $.ajax({
-            url: "http://ip-api.com/json",
+            url: "https://ipapi.co/json/",
             type: 'GET',
             dataType: 'json'
         });
@@ -31,7 +32,7 @@ function getLocation() {
                 resolve(geoLocation = request.response);
                 geoLocation = JSON.parse(geoLocation);
                 userCity = geoLocation.city;
-                countryCode = geoLocation.countryCode;
+                countryCode = geoLocation.country;
             } else {
                 reject(Error('There was an Error getting location'));
             }
@@ -57,8 +58,22 @@ function getWeather() {
 }
 
 function changeDOM() {
-    document.getElementById("weather-text").innerHTML = weatherData.weather[0].description;
+    console.log(weatherData);
+    $("#weather-text").html(weatherData.weather[0].description);
     tempKelvin = weatherData.main.temp;
     tempCelcius = tempKelvin - 273.15;
-    document.getElementById("degree").innerHTML = tempCelcius + " C";
+    tempFarenheit = tempCelcius * 1.8 + 32;
+    $("#degree").html(tempCelcius + "° C");
+    $('#city').html(userCity);
+    $('#icon').attr("src","http://openweathermap.org/img/w/"+ weatherData.weather[0].icon +".png")
+}
+
+function toggleUnits(){
+    if (isCelcius){
+        $("#degree").html(tempFarenheit + "° F");
+        isCelcius = false;
+    } else {
+        $("#degree").html(tempCelcius + "° C");
+        isCelcius = true;
+    }
 }
